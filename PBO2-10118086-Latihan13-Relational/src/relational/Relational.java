0ff54117e3f9ab32afbf1ce82b46c2b4134178be
damkina33 <55793563+damkina33@users.noreply.github.com>
2019-09-26 02:54:10 +0700

    Add files via upload

---
+++ PBO2-10118086-Latihan13-Relational/src/relational/Relational.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package relational;
+
+/**
+ *
+ * @author Sansan Juliana
+ */
+public class Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bill = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bill == bil2));
+        System.out.println("a != b =  " + (bill != bil2));
+        System.out.println("a > b = " + (bill > bil2));
+        System.out.println("a < b = " + (bill < bil2));
+        System.out.println("a >= b = " + (bill >= bil2));
+        System.out.println("a <= b = " + (bill <= bil2));
+        
+    }
+    
+}
