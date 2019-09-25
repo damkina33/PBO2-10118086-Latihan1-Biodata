0476f9bee016cee22600d9806e65d008519b70db
damkina33 <55793563+damkina33@users.noreply.github.com>
2019-09-26 02:51:37 +0700

    Add files via upload

---
+++ PBO2-10118086-Latihan4-Kambing/src/kambing/Kambing.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kambing;
+
+/**
+ *
+ * @author Anggi Juliana
+ */
+public class Kambing {
+    
+    public void tambahKambing() {
+        //Deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing Setelah Ditambah:" + jumlahKambing);
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Kambing kambingJantan = new Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
