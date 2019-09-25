342b979182fffc4cd084d710105d6ae7499b2f9c
damkina33 <55793563+damkina33@users.noreply.github.com>
2019-09-26 02:46:55 +0700

    Add files via upload

---
+++ PBO2-10118086-Latihan2-Deklarasi/src/latihan2/Latihan2.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2;
+
+/**
+ *
+ * @author Anggi Juliana
+ */
+public class Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta upppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan Hasil
+        System.out.println(); 
+        System.out.println("Isi Variabel Nilai = " + nilaiInt);
+        System.out.println("Isi Variabel PHI = " + PHI);
+        System.out.println("Isi Variabel logika = " + nilaiLogika);
+        System.out.println("Isi Variabel karakter = " + nilaiKarakter);
+    }
+    
+}
