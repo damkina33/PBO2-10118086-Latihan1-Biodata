7881a5525b534facd8b18224bd5bedf5fe636a0b
damkina33 <55793563+damkina33@users.noreply.github.com>
2019-09-26 02:54:21 +0700

    Add files via upload

---
+++ PBO2-10118086-Latihan15-Logika/src/logikalatihan15/LogikaLatihan15.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logikalatihan15;
+
+/**
+ *
+ * @author Sansan Juliana
+ */
+public class LogikaLatihan15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b =" + (a && b));
+        System.out.println("a || b =" + (a || b));
+        System.out.println("!a =" + !a);
+        System.out.println("!(a && b) =" + !(a && b));
+    }
+    
+}
