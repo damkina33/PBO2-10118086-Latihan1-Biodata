1d1514235551a95dc171ee0b52096db7dc74d5eb
damkina33 <55793563+damkina33@users.noreply.github.com>
2019-09-26 02:42:41 +0700

    Add files via upload

---
+++ PBO2-10118086-Latihan1-Biodata/src/biodata/Biodata.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata;
+
+/**
+ *
+ * @author Sansan Juliana
+ */
+public class Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("BIODATA SAYA" + "\n" + "-----------------------------");
+    System.out.println("NAMA\t\t: Anggi Juliana");
+    System.out.println("ALAMAT\t\t: Jl. Tubagus Ismail Dalam VIII no. 38");
+    System.out.println("TEMPAT LAHIR\t: Batam");
+    System.out.println("TANGGAL LAHIR\t: 29 Juli 1999");
+    System.out.println("-------------------------");
+    System.out.println("HOBI\t\t: Tidur, Ngudud dan Main Game");
+    System.out.println("MAKES\t\t: Samcan");
+    System.out.println("MIKES\t\t: Teh Leci");
+    }
+    
+}
